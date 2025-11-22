f6bcd361a7a725245c2b0d5e5f5bf39413527de8
Jaanvi Prabhakar <jaanviprabhakar5@gmail.com>
2025-11-22 18:48:14 -0500

    Add Superstore dataset and Assignment 2 analysis skeleton

---
+++ assignment2/tests/test_data_analysis.py
@@ -0,0 +1,31 @@
+from assignment2.src.data_analysis import (
+    load_csv,
+    total_sales,
+    sales_by_region,
+    sales_by_category,
+    top_selling_product,
+    profit_by_region,
+    daily_sales,
+    
+)
+
+def test_load_csv():
+    pass
+
+def test_total_sales():
+    pass
+
+def test_sales_by_region():
+    pass
+
+def test_sales_by_category():
+    pass
+
+def test_top_selling_product():
+    pass
+
+def test_profit_by_region():
+    pass
+
+def test_daily_sales():
+    pass
